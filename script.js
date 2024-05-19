3433f24c560c20d81fcddb58c4fc23c4dc46a73a
Akram Ahmed Khan <68415682+akram02-sde@users.noreply.github.com>
2024-05-19 11:26:35 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
+
+
 function firstNonRepeatedChar(str) {
     // Write your code here
+	if(str.length == 1){
+    return str
+	}
     for(let i = 0; i<str.length-1; i++){
         if(str[i] != str[i-1] && str[i] != str[i+1]){
             return str[i]
@@ -9,4 +14,4 @@ function firstNonRepeatedChar(str) {
     return null
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input));
+alert(firstNonRepeatedChar(input)); 
